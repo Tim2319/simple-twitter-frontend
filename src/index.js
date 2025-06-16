67f308fd7346ae2c5cfb27fe79c057933e8098c0
Tim2319 <destiny65740@gmail.com>
2025-06-16 15:37:37 +0800

    feat: add global axios token setup in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios'
+
+// 初始化 token：每次重新整理頁面就會設定一次
+const token = localStorage.getItem('token')
+if (token) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
