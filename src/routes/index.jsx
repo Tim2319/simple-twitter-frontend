04dcf12875077b8c3b1d54c1c72b0458033699da
Tim2319 <destiny65740@gmail.com>
2025-09-06 23:14:35 +0800

    add chatroom route path

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from '../pages/Login'
 import Home from '../pages/Home'
+import ChatRoom from '../pages/ChatRoom'
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/chat/:roomId" element={<ChatRoom />} /> 
     </Routes>
   )
 }
