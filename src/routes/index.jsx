e0252662e98028b58984bc269dc27e1c1b2a20f2
Tim2319 <destiny65740@gmail.com>
2025-09-30 16:06:29 +0800

    feat:add MessageLayout routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,14 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from '../pages/Login'
 import Home from '../pages/Home'
+import MessagesLayout from '../components/MessagesLayout'
 import ChatRoom from '../pages/Chatroom'
-
+import Layout from '../components/Layout'
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/chat/:roomId" element={<ChatRoom />} /> 
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+      
+      {/* Chatroom main route */}
+      <Route path="/chat" element={<MessagesLayout />}>
+          <Route index element={<div style={{ padding:16 }}>請選擇聊天室</div>} />
+          <Route path=":roomId" element={<ChatRoom />} />
+    </Route>
     </Routes>
   )
 }
